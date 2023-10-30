361c1a036ce26e2b2117dbbf04caaddbb813770f
gabriel <gabrieltrouve5@gmail.com>
2023-10-30 13:21:19 +0100

    add fields in UserCreationForm

--- docollective/accounts/forms.py
+++ docollective/accounts/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, forms
 from captcha.fields import ReCaptchaField
 
 
@@ -8,4 +8,4 @@ class ExChangerSignupForm(UserCreationForm):
 
     class Meta:
         model = get_user_model()
-        fields = ["username", "email", "first_name", "last_name"]
+        fields = ["username", "email", "first_name", "last_name", "type", "size", "favorite_color"]
