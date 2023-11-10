cb679a33a32ea34ae6353b19ab047a0585988a57
gabriel <gabrieltrouve5@gmail.com>
2023-11-10 23:16:37 +0100

    custom admin view Garment

--- docollective/shop/admin.py
+++ docollective/shop/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Color, Garment, Cart, Order
 
 admin.site.register(Color)
-admin.site.register(Garment)
 admin.site.register(Cart)
 admin.site.register(Order)
+
+
+@admin.register(Garment)
+class GarmentAdmin(admin.ModelAdmin):
+    list_display = ["description", "user", "published", "activate", "bought"]
+    list_editable = ["activate"]
+    list_display_links = ["description"]
+    list_filter = ["activate", "bought"]
