7b349f141ef8cb7ccf5f0a929fd9c1bda8aaa204
gabriel <gabrieltrouve5@gmail.com>
2023-11-16 01:28:24 +0100

    delete import

--- docollective/accounts/forms.py
+++ docollective/accounts/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, forms
+from django.contrib.auth.forms import UserCreationForm
 from captcha.fields import ReCaptchaField
 
 
