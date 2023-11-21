b439aad00905c178e26b11525b6fe4083549441c
gabriel <gabrieltrouve5@gmail.com>
2023-11-21 23:24:52 +0100

    custom admin for shop

--- docollective/shop/admin.py
+++ docollective/shop/admin.py
@@ -3,7 +3,6 @@ from .models import Color, Garment, Cart, Order
 
 admin.site.register(Color)
 admin.site.register(Cart)
-admin.site.register(Order)
 
 
 @admin.register(Garment)
@@ -12,3 +11,12 @@ class GarmentAdmin(admin.ModelAdmin):
     list_editable = ["activate"]
     list_display_links = ["description"]
     list_filter = ["activate", "bought"]
+    search_fields = ["description__icontains", "user__email__icontains"]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["user", "ordered", "ordered_date", "validation", "reference"]
+    list_editable = ["validation"]
+    list_filter = ["ordered", "validation"]
+    search_fields = ["user", "reference"]
