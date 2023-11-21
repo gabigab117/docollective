68470e969afdabbcabc2d71b3c2bd3af9e8857bc
gabriel <gabrieltrouve5@gmail.com>
2023-11-21 23:10:18 +0100

    add features in ModelAdmin for user

--- docollective/accounts/admin.py
+++ docollective/accounts/admin.py
@@ -5,6 +5,9 @@ from .models import ExChanger, ExChangerAdresses
 @admin.register(ExChanger)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "is_active", "is_superuser"]
+    list_editable = ["is_active", "is_superuser"]
+    list_filter = ["is_active"]
+    search_fields = ["first_name", "last_name"]
 
 
 @admin.register(ExChangerAdresses)
