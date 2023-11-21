c69051c88ca7aca80df36b549d7e2d4390b7ba92
gabriel <gabrieltrouve5@gmail.com>
2023-11-21 22:00:15 +0100

    display fields in adresses

--- docollective/accounts/admin.py
+++ docollective/accounts/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import ExChanger, ExChangerAdresses
 
 admin.site.register(ExChanger)
-admin.site.register(ExChangerAdresses)
+
+
+@admin.register(ExChangerAdresses)
+class AdressesAdmin(admin.ModelAdmin):
+    fields = ["user", "name", "address_1", "address_2", ("zip_code", "city"), "country", "default"]
 
