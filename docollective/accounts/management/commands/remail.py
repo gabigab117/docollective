a31036eaa9546dbe319dc76bf430f9d2d0c1cfbd
gabriel <gabrieltrouve5@gmail.com>
2023-11-24 01:33:44 +0100

    add a command in manager remail

---
+++ docollective/accounts/management/commands/remail.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.mail import EmailMessage
+from shop.models import Order
+from django.utils import timezone
+from datetime import timedelta
+
+
+class Command(BaseCommand):
+    help = "Re send an email to users who ordered more than 15 days ago and whose order is not validated"
+
+    def handle(self, *args, **options):
+        try:
+            orders = Order.objects.filter(ordered_date__lt=timezone.now() - timedelta(days=15))
+            for order in orders:
+                subject = f"Docoville, commande {order.reference}"
+                receiver = order.user.email
+                content = (f"Bonjour, nous n'avons toujours pas reçu votre "
+                           f"vêtement en échange de {order.garment.description}. Cordialement")
+                email = EmailMessage(
+                    subject, content, 'gabrieltrouve5@gmail.com', [receiver]
+                )
+                email.send()
+        except Exception as e:
+            raise CommandError(e)
