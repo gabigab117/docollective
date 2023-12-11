db0361819c54614e665a8efcbd4499cf33160085
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-12 00:29:17 +0100

    update urls

--- docollective/docollective/urls.py
+++ docollective/docollective/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
 
 if not settings.ENV == "PROD":
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 admin.site.site_header = 'Docollective'
