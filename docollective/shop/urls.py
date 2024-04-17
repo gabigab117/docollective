bd74a7378a4ac448cc17de0931e055153d38ccda
gabigab117 <gabrieltrouve5@gmail.com>
2024-04-17 22:52:15 +0200

    add a dynamic search with htmx

--- docollective/shop/urls.py
+++ docollective/shop/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import detail_view, add_to_cart, cart_view, delete_garments_cart, delete_cart, CreateGarment, all_garments, \
     DeleteGarment, my_shop_view, validate_cart_view, address_choice_view, admin_deal_validation_view, recommendations_view, \
-    admin_advert_validation_view
+    admin_advert_validation_view, search_results_view
 
 app_name = "shop"
 urlpatterns = [
@@ -15,6 +15,7 @@ urlpatterns = [
     path("create-garment/", CreateGarment.as_view(), name="create"),
     path("delete-garment/<int:pk>/", DeleteGarment.as_view(), name="delete-garment"),
     path("all/", all_garments, name="all"),
+    path("search/", search_results_view, name="search"),
     path("recommendations/", recommendations_view, name="recommendations"),
     path("my-shop/", my_shop_view, name="my-shop"),
     path("admin-validation/", admin_deal_validation_view, name="admin-validation"),
